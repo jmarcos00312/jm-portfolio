a3d75a9ec1e9ad6d04968b8f5b7161444ec87f66
Jeremiah <jeremiahmarcos27@outlook.com>
2023-04-11 13:25:58 -0500

    added and editted the about me section

--- src/Pages/Home.js
+++ src/Pages/Home.js
@@ -5,9 +5,13 @@ export default function Home() {
   return (
     <div className="home-page">
       <h1 className="welcome">Welcome</h1>
-      <div className="about-me">
-        About me
-      </div>
+      <p className="about-me">
+        As a <h4 id="career-title">full-stack developer</h4>, I have a strong background in deploying web applications using <h5>Ruby on Rails</h5>, <h5>Node</h5>, and Java <h5>JavaScript</h5>.
+        Through my experience in healthcare, I have honed my collaborative teamwork skills and developed the ability to work efficiently
+        under pressure to provide exceptional client service. My goal is to continually develop my skills and
+        expertise as a developer to help companies achieve
+        their business objectives.
+      </p>
       <div classname="social-logos">
         social media links and logos
       </div>
